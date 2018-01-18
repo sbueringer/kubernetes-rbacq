f21f1620283e3c46182765d707b05298f2f53037
Stefan Büringer <sbueringer@gmail.com>
2018-01-18 18:47:11 +0100

    update

--- cmd/get.go
+++ cmd/get.go
@@ -1,4 +1,4 @@
-// Copyright © 2017 Stefan Bueringer <sbueringer@gmail.com>
+// Copyright © 2018 Stefan Bueringer
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
